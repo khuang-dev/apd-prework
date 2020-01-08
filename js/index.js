2596b50f3e84e7e4d7f2b06b72af1bac340959d8
Ken <khuang.dev@gmail.com>
2020-01-07 17:07:02 -0800

    changed target for quiz-title buttons

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-(function($) {
-const title = $('.title');
-const content = $('.content');
+(function ($) {
 
-let data;
 
-$.get('src/quiz.json', function(d){
-    $.each(d.quizzes, function(key, value){
-            content.append(`<div>${value.title}</div>`);
+    
+    const content = $('.content');
+
+    $.get('src/quiz.json', function (d) {
+        $.each(d.quizzes, function (key, value) {
+            content.append(`<button class="quiz-title">${value.title}</button>`);
+        })
+        $('.quiz-title').on('click', function (e) {
+            console.log(e.target);
+            if (e.target.innerText === 'Abstract Quiz') {
+                console.log('abstract');
+            }
+            if (e.target.innerText === 'Dev Quiz') {
+                console.log('dev');
+            }
+        })
     })
-})
 
 })(jQuery);
