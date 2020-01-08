414e0d7e4e40baced535bcac573c78a2e2c1157f
Ken <khuang.dev@gmail.com>
2020-01-07 17:42:50 -0800

    added first question and answers on click

--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
 (function ($) {
 
 
-    
+
     const content = $('.content');
 
     $.get('src/quiz.json', function (d) {
+
         $.each(d.quizzes, function (key, value) {
             content.append(`<button class="quiz-title">${value.title}</button>`);
         })
+
         $('.quiz-title').on('click', function (e) {
-            console.log(e.target);
+            // content.empty();
             if (e.target.innerText === 'Abstract Quiz') {
                 console.log('abstract');
+                console.log(d.quizzes[0].questions[0].answers)
+                content.append(`<h2>${d.quizzes[0].questions[0].question}<h2>`)
+                $.each(d.quizzes[0].questions[0].answers, function (key, value) {
+                    content.append(`<button class="answers">${value.content}</button>`);
+                })
             }
+
             if (e.target.innerText === 'Dev Quiz') {
                 console.log('dev');
+                content.append(`<h2>${d.quizzes[1].questions[1].question}<h2>`)
+                $.each(d.quizzes[1].questions[1].answers, function (key, value) {
+                    content.append(`<button class="answers">${value.content}</button>`);
+                })
             }
+
         })
+
+
     })
 
 })(jQuery);
