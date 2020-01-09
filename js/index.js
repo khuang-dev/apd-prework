713552893aeb53f4c3f0d7a64576ecb5913fafaf
Ken <khuang.dev@gmail.com>
2020-01-08 18:06:20 -0800

    added score

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 (function ($) {
 
     const content = $('.content');
-    const nav = $('nav');
-
-
-
 
     $.get('src/quiz.json', function (d) {
 
@@ -14,30 +10,32 @@
 
         $('.quiz-title').on('click', function (e) {
 
+            $('.title').hide();
+            $('.score-title').show();
+            let scoreCount = 0;
+            let score = $('.score');
+            score.append(scoreCount);
+
             // QUIZ 1
             $('.quiz-title').hide();
-            // content.empty();
             if (e.target.innerText === 'Abstract Quiz') {
-                console.log('abstract');
-                console.log(d.quizzes[0].questions[2])
                 content.append(`<h2>${d.quizzes[0].questions[0].question}<h2>`)
                 $.each(d.quizzes[0].questions[0].answers, function (key, value) {
                     content.append(`<button class="answers ${value.value}">${value.content}</button>`);
                 })
                 const answer = $('.answers');
-                const correct = $('.true');
-                const incorrect = $('.false');
-                answer.on('click', function (e) {
-                    if (e.target.classList[1] === 'true') {
-                        correct.css('background-color', 'green');
-                    } else (e.target.classList[1] === 'false'); {
-                        incorrect.css('background-color', 'red');
-                        correct.css('background-color', 'green');
+
+                answer.one('click', function (e) {
+                    answer.attr('disabled', 'disabled');
+                    if ($(this)[0].classList[1] === 'true') {
+                        $(this).css('background-color', 'green');
+                        score.html(function(i, val) { return val*1+1 });
+                    } else {
+                        $(this).css('background-color', 'red');
                     }
-                    console.dir(e.target)
                     $('.abstract-q-one').show();
                 })
-                
+
                 $('.abstract-q-one').on('click', function () {
                     $('.abstract-q-one').hide();
                     content.empty();
@@ -46,18 +44,17 @@
                         content.append(`<button class="answers ${value.value}">${value.content}</button>`);
                     })
                     const answer = $('.answers');
-                    const correct = $('.true');
-                    const incorrect = $('.false');
-                    answer.on('click', function (e) {
-                        if (e.target.classList[1] === 'true') {
-                            correct.css('background-color', 'green');
-                        } else (e.target.classList[1] === 'false'); {
-                            incorrect.css('background-color', 'red');
-                            correct.css('background-color', 'green');
+                    answer.one('click', function (e) {
+                        answer.attr('disabled', 'disabled');
+                        if ($(this)[0].classList[1] === 'true') {
+                            $(this).css('background-color', 'green');
+                            score.html(function(i, val) { return val*1+1 });
+                        } else {
+                            $(this).css('background-color', 'red');
                         }
                         $('.abstract-q-one').hide();
                         $('.abstract-q-two').show();
-                    })                    
+                    })
                 })
                 $('.abstract-q-two').on('click', function () {
                     $('.abstract-q-one').hide();
@@ -68,23 +65,24 @@
                         content.append(`<button class="answers ${value.value}">${value.content}</button>`);
                     })
                     const answer = $('.answers');
-                    const correct = $('.true');
-                    const incorrect = $('.false');
-                    answer.on('click', function (e) {
-                        if (e.target.classList[1] === 'true') {
-                            correct.css('background-color', 'green');
-                        } else (e.target.classList[1] === 'false'); {
-                            incorrect.css('background-color', 'red');
-                            correct.css('background-color', 'green');
+                    answer.one('click', function (e) {
+                        answer.attr('disabled', 'disabled');
+                        if ($(this)[0].classList[1] === 'true') {
+                            $(this).css('background-color', 'green');
+                            score.html(function(i, val) { return val*1+1 });
+                        } else {
+                            $(this).css('background-color', 'red');
                         }
                         $('.abstract-q-one').hide();
                         $('.abstract-q-two').hide();
                         $('.abstract-results').show();
-                    })                    
+                    })
                 })
-                $('.abstract-results').on('click', function(){
+                $('.abstract-results').on('click', function () {
                     content.empty();
-                    //if score > 50% pass, else if score < 50% fail
+                    $('.abstract-results').hide();
+                    $('.score-title').hide();
+                    content.append('<h1>Your score is ' + score[0].innerText + ".")
                 })
             }
 
