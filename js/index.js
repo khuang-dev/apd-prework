6a24f98afb6cfec754e5e560a113e295ecaa46dd
Ken <khuang.dev@gmail.com>
2020-01-08 18:56:06 -0800

    added quiz two

--- js/index.js
+++ js/index.js
@@ -1,43 +1,54 @@
 (function ($) {
 
+// Variables
     const content = $('.content');
+    const title = $('.title');
+    const scoreTitle = $('.score-title');
+    const qOne = $('.question-one');
+    const qTwo = $('.question-two');
+    const Results = $('.results');
 
-    $.get('src/quiz.json', function (d) {
+    $.get('src/quiz.json', function (d) { //fetch data
 
-        $.each(d.quizzes, function (key, value) {
+        $.each(d.quizzes, function (key, value) { // loop for quiz title
             content.append(`<button class="quiz-title">${value.title}</button>`);
         })
 
-        $('.quiz-title').on('click', function (e) {
+        const quizTitle = $('.quiz-title');
+        quizTitle.on('click', function (e) { // click to select a quiz
 
-            $('.title').hide();
-            $('.score-title').show();
-            let scoreCount = 0;
+            title.hide();
+            scoreTitle.show();
+
+            let scoreCount = 0; // score counter
             let score = $('.score');
             score.append(scoreCount);
 
             // QUIZ 1
-            $('.quiz-title').hide();
-            if (e.target.innerText === 'Abstract Quiz') {
+            quizTitle.hide();
+
+            if (e.target.innerText === 'Abstract Quiz') { // check for which quiz, loop for question one
                 content.append(`<h2>${d.quizzes[0].questions[0].question}<h2>`)
-                $.each(d.quizzes[0].questions[0].answers, function (key, value) {
+
+                $.each(d.quizzes[0].questions[0].answers, function (key, value) { // loop for answers to question one
                     content.append(`<button class="answers ${value.value}">${value.content}</button>`);
                 })
+
                 const answer = $('.answers');
 
-                answer.one('click', function (e) {
+                answer.one('click', function (e) { // click to select an answer
                     answer.attr('disabled', 'disabled');
-                    if ($(this)[0].classList[1] === 'true') {
+                    if ($(this)[0].classList[1] === 'true') { // check if answer is correct
                         $(this).css('background-color', 'green');
-                        score.html(function(i, val) { return val*1+1 });
-                    } else {
+                        score.html(function (i, val) { return val * 1 + 1 }); // add 1 point to score count
+                    } else { // check if answer is incorrect
                         $(this).css('background-color', 'red');
                     }
-                    $('.abstract-q-one').show();
+                    qOne.show();
                 })
 
-                $('.abstract-q-one').on('click', function () {
-                    $('.abstract-q-one').hide();
+                qOne.on('click', function () { // loop for Q&A for question two
+                    qOne.hide();
                     content.empty();
                     content.append(`<h2>${d.quizzes[0].questions[1].question}<h2>`)
                     $.each(d.quizzes[0].questions[1].answers, function (key, value) {
@@ -48,17 +59,17 @@
                         answer.attr('disabled', 'disabled');
                         if ($(this)[0].classList[1] === 'true') {
                             $(this).css('background-color', 'green');
-                            score.html(function(i, val) { return val*1+1 });
+                            score.html(function (i, val) { return val * 1 + 1 });
                         } else {
                             $(this).css('background-color', 'red');
                         }
-                        $('.abstract-q-one').hide();
-                        $('.abstract-q-two').show();
+                        qOne.hide();
+                        qTwo.show();
                     })
                 })
-                $('.abstract-q-two').on('click', function () {
-                    $('.abstract-q-one').hide();
-                    $('.abstract-q-two').hide();
+                qTwo.on('click', function () { // loop for Q&A for question three
+                    qOne.hide();
+                    qTwo.hide();
                     content.empty();
                     content.append(`<h2>${d.quizzes[0].questions[2].question}<h2>`)
                     $.each(d.quizzes[0].questions[2].answers, function (key, value) {
@@ -69,36 +80,106 @@
                         answer.attr('disabled', 'disabled');
                         if ($(this)[0].classList[1] === 'true') {
                             $(this).css('background-color', 'green');
-                            score.html(function(i, val) { return val*1+1 });
+                            score.html(function (i, val) {
+                                return val * 1 + 1
+                            });
                         } else {
                             $(this).css('background-color', 'red');
                         }
-                        $('.abstract-q-one').hide();
-                        $('.abstract-q-two').hide();
-                        $('.abstract-results').show();
+                        qOne.hide();
+                        qTwo.hide();
+                        Results.show();
                     })
                 })
-                $('.abstract-results').on('click', function () {
+                Results.on('click', function () { // show results
                     content.empty();
-                    $('.abstract-results').hide();
-                    $('.score-title').hide();
-                    content.append('<h1>Your score is ' + score[0].innerText + ".")
+                    Results.hide();
+                    scoreTitle.hide();
+                    if (score[0].innerText >= 2) {
+                        content.append('<div><h1>You passed!</h1><h2>Your score is ' + score[0].innerText + ".</h2></div>")
+                    } else {
+                        content.append('<div><h1>You failed!</h1><h2>Your score is ' + score[0].innerText + ".</h2></div>")
+                    }
                 })
             }
-
-
 
             // QUIZ 2
-            if (e.target.innerText === 'Dev Quiz') {
-                console.log('dev');
-                content.append(`<h2>${d.quizzes[1].questions[1].question}<h2>`)
-                $.each(d.quizzes[1].questions[1].answers, function (key, value) {
-                    content.append(`<button class="answers">${value.content}</button>`);
+            if (e.target.innerText === 'Dev Quiz') { // check for which quiz, loop for question one
+                content.append(`<h2>${d.quizzes[1].questions[0].question}<h2>`)
+
+                $.each(d.quizzes[1].questions[0].answers, function (key, value) { // loop for answers to question one
+                    content.append(`<button class="answers ${value.value}">${value.content}</button>`);
+                })
+
+                const answer = $('.answers');
+
+                answer.one('click', function (e) { // click to select an answer
+                    answer.attr('disabled', 'disabled');
+                    if ($(this)[0].classList[1] === 'true') { // check if answer is correct
+                        $(this).css('background-color', 'green');
+                        score.html(function (i, val) { return val * 1 + 1 }); // add 1 point to score count
+                    } else { // check if answer is incorrect
+                        $(this).css('background-color', 'red');
+                    }
+                    qOne.show();
+                })
+
+                qOne.on('click', function () { // loop for Q&A for question two
+                    qOne.hide();
+                    content.empty();
+                    content.append(`<h2>${d.quizzes[1].questions[1].question}<h2>`)
+                    $.each(d.quizzes[1].questions[1].answers, function (key, value) {
+                        content.append(`<button class="answers ${value.value}">${value.content}</button>`);
+                    })
+                    const answer = $('.answers');
+                    answer.one('click', function (e) {
+                        answer.attr('disabled', 'disabled');
+                        if ($(this)[0].classList[1] === 'true') {
+                            $(this).css('background-color', 'green');
+                            score.html(function (i, val) { return val * 1 + 1 });
+                        } else {
+                            $(this).css('background-color', 'red');
+                        }
+                        qOne.hide();
+                        qTwo.show();
+                    })
+                })
+                qTwo.on('click', function () { // loop for Q&A for question three
+                    qOne.hide();
+                    qTwo.hide();
+                    content.empty();
+                    content.append(`<h2>${d.quizzes[1].questions[2].question}<h2>`)
+                    $.each(d.quizzes[1].questions[2].answers, function (key, value) {
+                        content.append(`<button class="answers ${value.value}">${value.content}</button>`);
+                    })
+                    const answer = $('.answers');
+                    answer.one('click', function (e) {
+                        answer.attr('disabled', 'disabled');
+                        if ($(this)[0].classList[1] === 'true') {
+                            $(this).css('background-color', 'green');
+                            score.html(function (i, val) {
+                                return val * 1 + 1
+                            });
+                        } else {
+                            $(this).css('background-color', 'red');
+                        }
+                        qOne.hide();
+                        qTwo.hide();
+                        Results.show();
+                    })
+                })
+                Results.on('click', function () { // show results
+                    content.empty();
+                    Results.hide();
+                    scoreTitle.hide();
+                    if (score[0].innerText >= 2) {
+                        content.append('<div><h1>You passed!</h1><h2>Your score is ' + score[0].innerText + ".</h2></div>")
+                    } else {
+                        content.append('<div><h1>You failed!</h1><h2>Your score is ' + score[0].innerText + ".</h2></div>")
+                    }
                 })
             }
-
         })
-
 
     })
 
